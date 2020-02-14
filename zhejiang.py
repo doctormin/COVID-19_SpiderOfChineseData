155e78d2f16c30a0a9caf39ac7214d610e949ea2
YiminZhao <doctormin@sjtu.edu.cn>
2020-02-14 22:28:52 +0800

    .

--- zhejiang.py
+++ zhejiang.py
@@ -6,20 +6,13 @@
 #By Bugen Zhao
 import re
 from selenium import webdriver
-
 driver = webdriver.Chrome() # Switch to Chrome if necessary
-
-
 # In[34]:
 url = 'http://www.zjwjw.gov.cn/col/col1202101/index.html'
 info_pattern = re.compile(r'.*浙江.*新.*冠.*疫情.*情况')
 info_url = ''
 info_title = ''
-
-
 # In[35]:
-
-
 driver.get(url)
 nodes = driver.find_elements_by_xpath('//*[@id="4978845"]/div/li')
 for node in nodes:
@@ -28,18 +21,10 @@ for node in nodes:
         info_title = title
         info_url = node.find_element_by_xpath('a').get_attribute('href')
         break
-
-
 # In[36]:
-
-
 driver.get(info_url)
 info_content = driver.find_element_by_xpath('//*[@id="zoom"]').text.strip()
-
-
 # In[38]:
-
-
 print(info_title)
 print(info_url)
 print(info_content)
